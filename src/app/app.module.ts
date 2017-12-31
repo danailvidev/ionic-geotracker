4dcdc39e9028de3f61894972a4c76e32ced6c528
danailvidev <danailvidev@gmail.com>
2017-12-31 15:20:19 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { Geolocation } from '@ionic-native/geolocation';
+import { Device } from '@ionic-native/device';
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   imports: [
     BrowserModule,
@@ -19,12 +22,14 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation,
+    Device
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,246 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Device } from '@ionic-native/device';
+import * as firebase from 'firebase';
+declare var google: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  isAndroid: boolean = false;
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  markers: any = [];
+  ref = firebase.database().ref('geolocations/');
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform,
+    private geolocation: Geolocation,
+    private device: Device
+  ) {
+    this.isAndroid = platform.is('android');
+    platform.ready().then(() => {
+      this.initMap();
+    });
 
+    this.ref.on('value', resp => {
+      this.deleteMarkers();
+      snapshotToArray(resp).forEach(data => {
+        if (data.uuid !== this.device.uuid) {
+          let image = 'assets/imgs/blue-marker.png';
+          let updatelocation = new google.maps.LatLng(data.latitude, data.longitude);
+          this.addMarker(updatelocation, image);
+          this.setMapOnAll(this.map);
+        } else {
+          let image = 'assets/imgs/red-marker.png';
+          let updatelocation = new google.maps.LatLng(data.latitude, data.longitude);
+          this.addMarker(updatelocation, image);
+          this.setMapOnAll(this.map);
+        }
+      });
+    });
+  }
+
+  initMap() {
+    this.geolocation.getCurrentPosition({ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }).then((resp) => {
+      let mylocation = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        zoom: 15,
+        center: mylocation,
+        disableDefaultUI: true,
+        gestureHandling: 'cooperative',
+        styles: [{
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#f5f5f5"
+            }]
+          },
+          {
+            "elementType": "labels.icon",
+            "stylers": [{
+              "visibility": "off"
+            }]
+          },
+          {
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#616161"
+            }]
+          },
+          {
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+              "color": "#f5f5f5"
+            }]
+          },
+          {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#bdbdbd"
+            }]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#eeeeee"
+            }]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#757575"
+            }]
+          },
+          {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#e5e5e5"
+            }]
+          },
+          {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#9e9e9e"
+            }]
+          },
+          {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#ffffff"
+            }]
+          },
+          {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#757575"
+            }]
+          },
+          {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#dadada"
+            }]
+          },
+          {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#616161"
+            }]
+          },
+          {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#9e9e9e"
+            }]
+          },
+          {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#e5e5e5"
+            }]
+          },
+          {
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#eeeeee"
+            }]
+          },
+          {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{
+              "color": "#c9c9c9"
+            }]
+          },
+          {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+              "color": "#9e9e9e"
+            }]
+          }
+        ]
+      });
+    });
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      this.deleteMarkers();
+      this.updateGeolocation(this.device.uuid, data.coords.latitude, data.coords.longitude);
+      let updatelocation = new google.maps.LatLng(data.coords.latitude, data.coords.longitude);
+      let image = 'assets/imgs/red-marker.png';
+      this.addMarker(updatelocation, image);
+      this.setMapOnAll(this.map);
+    });
+  }
+
+  addMarker(location, image) {
+    let marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      icon: image
+    });
+    this.markers.push(marker);
+  }
+
+  setMapOnAll(map) {
+    for (var i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(map);
+    }
+  }
+
+  clearMarkers() {
+    this.setMapOnAll(null);
+  }
+
+  deleteMarkers() {
+    this.clearMarkers();
+    this.markers = [];
+  }
+
+  updateGeolocation(uuid, lat, lng) {
+    if (localStorage.getItem('dvgeo')) {
+      firebase.database().ref('geolocations/' + localStorage.getItem('dvgeo')).set({
+        uuid: uuid,
+        latitude: lat,
+        longitude: lng
+      });
+    } else {
+      let newData = this.ref.push();
+      newData.set({
+        uuid: uuid,
+        latitude: lat,
+        longitude: lng
+      });
+      localStorage.setItem('dvgeo', newData.key);
+    }
   }
 
 }
+
+export const snapshotToArray = snapshot => {
+  let returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+    let item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+
+  return returnArr;
+};
